1ac6f5b30e6691d87b1924d8d186d726a414ddb2
alxs1dd93 <alxs1dd93@gmail.com>
2021-02-10 04:08:47 +0300

    Готовый черновой вариант десктопа

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        // свойства и их значения слайдера
+        // стрелки true/false
+        arrows: false,
+        // точки
+        dots: true,
+        //алдаптивная высота
+        // adaptiveHeight: true,
+        // сколько показывать на 1 странице
+        slidesToShow: 3,
+        //сколько слайдов прокручивать за раз
+        slidesToScroll: 3,
+        // скорость перелистывания
+        speed: 500,
+        // анимация
+        // easing: 'linear',
+        // конечность слайдера true/false
+        // infinite: true,
+        // стартовый слайдер
+        // initialSlide: 0,
+        // автоплей
+        autoplay: false,
+        autoplaySpeed: 5500, // скорость автопрокрутки 0.5сек
+        //паузы
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        //возможность: 1)переворота по самому слайду 2) тачем
+        draggable: true,
+        swipe: true,
+        //ждать ли анимацию
+        waitForAnimate: false,
+        // центровка слайда 31:13
+        centerMode: false,
+        variableWidth: false,
+        //ряды
+        rows: 1,
+        //вертикальный вариант
+        // vertical: true,
+        //АДАПТИВ слайдера
+        // responsive: [
+        //     {
+        //         breakpoint: 950,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 1,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 930,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1,
+        //         }
+        //     }
+        // ]
+        
+
+    });
+  });
